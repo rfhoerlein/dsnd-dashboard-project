213d173ebc111613a579a9daf007e93c9748401d
rfhoerlein <rfhoerlein@gmail.com>
2025-08-10 14:19:17 -0400

    fixed __init__

--- python-package/employee_events/__init__.py
+++ python-package/employee_events/__init__.py
@@ -1,4 +1,4 @@
-from .employee import Employee #noqa
-from .team import Team #noqa
-from .query_base import QueryBase #noqa
-from .sql_execution import * #noqa 
+from .employee import Employee # noqa
+from .team import Team # noqa
+from .query_base import QueryBase # noqa
+from .sql_execution import * # snoqa 
